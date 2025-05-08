82f526be0fdfa7da183e5c3235e341f2b8c51f95
farhanfath <farhanfathur73@gmail.com>
2025-05-08 21:25:47 +0700

    add data

--- app/src/main/java/pi/project/grapify/data/model/DiseaseInfo.kt
+++ app/src/main/java/pi/project/grapify/data/model/DiseaseInfo.kt
@@ -1,6 +1,7 @@
 package pi.project.grapify.data.model
 
 data class DiseaseInfo(
+    val nama: String,
     val penyebab: String,
     val gejala: String,
     val pencegahan: String
--- app/src/main/java/pi/project/grapify/domain/util/DataConstants.kt
+++ app/src/main/java/pi/project/grapify/domain/util/DataConstants.kt
@@ -8,26 +8,31 @@ import pi.project.grapify.data.model.DiseaseInfo
 fun getDiseaseInfo(diseaseName: String): DiseaseInfo? {
     return when (diseaseName) {
         "Grape Black Rot" -> DiseaseInfo(
+            nama = "Grape Black Rot",
             penyebab = "Penyakit ini disebabkan oleh jamur Phyllosticta ampelicida. Patogen tersebut biasanya menyerang saat musim dingin dan bertahan hidup pada pucuk yang terinfeksi atau buah kering yang terdapat pada tanaman anggur maupun di tanah. Spora jamur dilepaskan ketika terjadi hujan ringan dan kemudian tersebar melalui angin. Suhu optimal untuk pertumbuhan jamur ini adalah sekitar 25°C dengan kelembapan pada daun yang bertahan setidaknya selama enam jam.",
             gejala = "Gejala penyakit ini ditandai dengan munculnya bintik-bintik tidak beraturan yang dikelilingi oleh garis berwarna gelap merupakan salah satu gejala pada daun. Selain daun, gejala serupa juga dapat terlihat pada tunas, batang, dan tangkai daun. Jika infeksi terjadi pada tangkai daun, maka daun secara keseluruhan akan mengalami pengeringan.",
             pencegahan = "• Gunakan metode pemangkasan alternatif seperti pemangkasan tertunda atau pemangkasan ganda.\n• Hindari pemangkasan selama periode hujan deras ketika spora cenderung tersebar.\n• Pantau kebun anggur di musim semi, dan cari tunas yang mati atau tunas yang kerdil.\n• Pada akhir musim panas, potong bagian tanaman anggur yang membusuk.\n• Singkirkan sisa-sisa tanaman anggur yang terserang penyakit dan musnahkan.\n• Tunda pembuahan selama beberapa tahun hingga tanaman merambat memiliki pertumbuhan akar dan tunas yang seimbang."
         )
         "Grape Esca (Black Measles)" -> DiseaseInfo(
+            nama = "Grape Esca (Black Measles)",
             penyebab = "Penyakit ini dapat muncul kapan saja selama musim tanam dan disebabkan oleh jamur Togninia minima, meskipun beberapa jenis jamur lain seperti Phaeomoniella chlamydospora juga dapat berperan. Infeksi umumnya terjadi pada tanaman anggur yang masih muda, namun gejalanya sering baru tampak di kebun anggur saat tanaman berusia antara 5 hingga 7 tahun.",
             gejala = "Gejala penyakit ini ditandai dengan munculnya belang di antara pembuluh daun, yang disertai perubahan warna serta pengeringan jaringan di sekitar pembuluh utama. Pada varietas anggur berbuah merah, perubahan warna umumnya menjadi merah tua, sedangkan pada varietas berbuah putih, warna berubah menjadi kuning. Daun yang terinfeksi dapat mengalami pengeringan total dan rontok sebelum waktunya.",
             pencegahan = "• Pilih varietas anggur yang lebih tahan penyakit jika tersedia.\n• Buang buah kering (mumi) dari pohon anggur.\n• Singkirkan dan musnahkan kayu serta sulur yang terinfeksi hama setelah panen.\n• Singkirkan daun-daun yang terkena serangan dari area kebun anggur.\n• Jaga kebersihan kebun anggur dengan membersihkan gulma.\n• Pastikan sirkulasi udara dan pencahayaan di kebun cukup.\n• Lakukan pemangkasan tanaman anggur setiap tahun sebelum fase vegetatif dimulai."
         )
         "Grape Leaf Blight (Isariopsis Leaf Spot)" -> DiseaseInfo(
+            nama = "Grape Leaf Blight (Isariopsis Leaf Spot)",
             penyebab = "Penyakit ini disebabkan oleh jamur Peyronellaea glomerata, yang sebelumnya dikenal sebagai Phoma glomerata. Jamur ini tersebar luas dan mampu bertahan di tanah maupun pada berbagai bahan tanaman, baik yang masih hidup maupun sudah mati (seperti biji, buah-buahan, dan sayuran), biasanya tanpa menimbulkan gejala yang nyata.",
             gejala = "Gejala infeksi hawar daun phoma umumnya terlihat pada daun-daun yang lebih tua. Daun yang terinfeksi memperlihatkan lesi tidak teratur berbentuk sudut dengan warna kuning hingga cokelat yang menyebar di seluruh permukaan daun. Seiring perkembangan penyakit, lesi tersebut membesar membentuk bercak yang kemudian berubah menjadi area nekrotik dengan warna kusam, pusat berwarna abu-abu, dan tepi yang lebih gelap.",
             pencegahan = "• Memilih varietas tanaman yang tahan terhadap hawar daun.\n• Singkirkan daun yang terinfeksi dan gugur.\n• Menjaga kebun tetap bersih dari gulma dan sisa tanaman.\n• Menyediakan sirkulasi udara dan pencahayaan yang cukup antar tanaman.\n• Menghindari penyiraman dari atas yang membasahi daun.\n• Membersihkan alat pertanian dan tempat penyimpanan secara berkala.\n• Menggunakan fungisida sesuai kebutuhan dan anjuran.\n• Memantau tanaman secara berkala untuk deteksi dini infeksi."
         )
         "Grape Healthy" -> DiseaseInfo(
+            nama = "Grape Healthy",
             penyebab = "Daun anggur sehat tidak terinfeksi oleh patogen penyebab penyakit.",
             gejala = "Kondisi daun anggur terlihat normal dan tidak menunjukan tanda-tanda penyakit.",
             pencegahan = "• Lanjutkan perawatan rutin seperti penyiraman, pemangkasan, dan pemupukan.\n • Tetap pantau kondisi secara berkala. \n• Memastikan nutrisi tanah yang seimbang.\n• Melakukan pemangkasan secara teratur untuk sirkulasi udara yang baik.\n• Menjaga irigasi yang tepat dan menghindari kelebihan air.\n• Melakukan rotasi tanaman dan sanitasi kebun yang baik.\n• Pemantauan hama dan penyakit secara berkala.\n• Menggunakan varietas anggur yang tahan terhadap penyakit umum di daerah tersebut."
         )
         "Not Anggur" -> DiseaseInfo(
+            nama = "Not Anggur",
             penyebab = "Gambar yang diunggah bukan daun anggur atau tidak dapat diidentifikasi sebagai daun anggur.",
             gejala = "Tidak relevan karena bukan daun anggur.",
             pencegahan = "• Pastikan foto yang diunggah jelas, fokus, dan menunjukan daun secara utuh \n • Coba ulangi dengan sudut pengambilan gambar yang berbeda \n • Pastikan gambar berasal dari tanaman anggur"
